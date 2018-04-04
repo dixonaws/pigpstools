3c611511e3f795982a75025020b298651e5fe7d4
dixonaws <dixonaws@amazon.com>
2018-04-03 20:00:02 -0400

    added sleep(1) to read program

--- readcoordinates.py
+++ readcoordinates.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
 import memcache
+from time import sleep
 
 mc = memcache.Client(['127.0.0.1:11211'], debug=0)
 
+# read the GPS sentences once per second from the local memcached service
 while True:
 	strGPRMC=mc.get("GPRMC")
 	strGPGGA=mc.get("GPGGA")
@@ -13,5 +15,5 @@ while True:
 	print strGPGGA
 	print strGPVTG
 			
-
+	sleep(1)
 
